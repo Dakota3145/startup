8eaf6a79afe5dc2e00f6adfd5d7ef6e95386dac8
dakota807 <dakota31@byu.edu>
2023-06-14 10:41:25 -0600

    login page now has rest of html and javascript elements

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
+
+function login() {
+
+}
+
 export function Login({ userName }) {
   return (
     <main>
-        <h1>Login</h1>
+      <div style={{display: "flex", justifyContent: "center"}}>
+        <label htmlFor="loginUsername">Username:</label>
+        <input type="text" id="loginUsername" name="loginUsername" placeholder="Enter Username" /><br /><br />
+      </div>
+      <div style={{display: "flex", justifyContent: "center"}}>
+        <label htmlFor="loginPassword">Password:</label>
+        <input type="password" id="loginPassword" name="loginPassword" placeholder="Enter Password" /><br /><br />
+      </div>
+      <div style={{display: "flex", justifyContent: "center"}}>
+        <button className="btn btn-primary" onClick={() => login()} role="button">Login</button>
+        <button className="btn btn-primary" onClick={() => navigate('/signup')} role="button">No Account? Sign up Here</button>
+      </div>
     </main>
   );
 }
